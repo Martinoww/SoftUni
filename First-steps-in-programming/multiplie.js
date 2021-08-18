324281807d36aa2caed44ec1e91d10e8edbe08f5
Alexander Martinov <89145434+Martinoww@users.noreply.github.com>
2021-08-18 22:07:18 +0300

    Add files via upload
    Added my first steps in programming.

---
+++ First-steps-in-programming/multiplie.js
@@ -0,0 +1,7 @@
+function area(input){
+let a = Number(input[0])
+let result = a * a ;
+console.log(result);
+
+}
+area(['134'])
---
+++ First-steps-in-programming/nums1-10.js
@@ -0,0 +1,5 @@
+function count() {
+  for (let count = 1; count <= 10; count++)
+  console.log(count);
+}
+count();
